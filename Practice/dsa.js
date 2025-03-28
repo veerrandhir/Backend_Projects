c879c093ee45af94288a6b92445aa48f24755396
Randhir_Restart <veerrandhir3@gmail.com>
2025-03-28 13:20:14 +0530

    Moved to folder practice

---
+++ Practice/dsa.js
@@ -0,0 +1,36 @@
+// Calculate "Simple Interest" From principal(P), Rate(R), and Time(T).
+
+// let P = prompt("Enter the Principal Amount : ");
+// let R = prompt("What is Rate? :");
+// let T = prompt("Time period in years :");
+// let SI = P * R * T;
+// console.log("Your SI is ", SI);
+
+// Q Calculate Max of 2 numbers.
+// let a = prompt("Enter first number:");
+// let b = prompt("Enter second number :");
+// if (a < b) {
+//   console.log(b, "is Bigger number");
+// } else {console.log(a, "is bigger number")};
+// Q Calculate Factorial of a number N.
+// let num = prompt("Enter the number you want:");
+// let i = 1;
+// let fact = 1;
+// while (i <= num) {
+//   fact = fact*i;
+//   i++;
+// }
+// console.log(fact);
+
+// Q Given a person's age, find if they should get a driving licence or not.
+// let age = prompt("Enter Your Age:");
+// if (age < 18) {
+//   console.log("Sorry You are not eligible to apply for Driving licence");
+// } else {
+//   let name = prompt("Enter your Full name:");
+//   let DOB = prompt("Enter your D.O.B:");
+//   console.log('"Congrats" You are eligible');
+//   console.log(
+//     `User details ${age}, ${name},${DOB}\n ${name} You can Apply Now`
+//   );
+// }
